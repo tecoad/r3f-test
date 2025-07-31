db7d5d2c488483379e88ec3d8d7ac673e084992d
Matheus <tecoad@gmail.com>
2025-07-30 23:55:28 -0300

    Update layout and add instructional text in main route component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,11 +7,14 @@ export const Route = createFileRoute("/")({
 
 function App() {
 	return (
-		<div className="min-h-svh flex gap-4 flex-col justify-center items-center z-50 relative border border-red-500 h-[2000px]">
+		<div className="min-h-svh flex gap-4 flex-col  z-50 relative border border-red-500 h-[2000px]">
+			<span className="text-white mt-10 ml-10">
+				Scroll down and click on the element
+			</span>
 			<Link to="/pageb">
 				<AnimatedElement
 					id="shared-element"
-					className="w-20 h-20 absolute top-[1000px] left-0"
+					className="w-20 h-20 absolute top-[1000px] left-10"
 				/>
 			</Link>
 		</div>
